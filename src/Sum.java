6b30a1932443a89c384c295de1dbd45950d73b55
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2023-05-18 11:23:12 +0545

    multiplication feature added

---
+++ src/Sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+    public static void main(String[] args) {
+        int a=2;
+        int b=3;
+        int result;
+        result=a+b;
+        System.out.println("The sum is "+ result +" meters");
+    }
+
+}
---
+++ src/multiply.java
@@ -0,0 +1,9 @@
+public class multiply {
+    public static void main(String[] args) {
+        int a=5;
+        int b=15;
+        int result;
+        result=a*b;
+        System.out.println("the multiplication is " + result);
+    }
+}
