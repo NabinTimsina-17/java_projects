4590d68ccc184d815f3510d2ac32dc5fe2c3628b
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2023-05-18 20:54:31 +0545

    learned polymorphism...

--- src/Helper.java
+++ src/Helper.java
@@ -19,9 +19,6 @@ class GFG {
     public static void main(String[] args)
     {
 
-
-
-
         System.out.println(Helper.Multiply(2, 4));
         System.out.println(Helper.Multiply(5.5, 6.3));
     }
